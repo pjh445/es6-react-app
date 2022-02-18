b4747a3393fb1abb9f3f439ea3298d17b2a0073f
jeonghwa,park <pjh445@gmail.com>
2022-02-18 15:33:26 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 
 const myArray = ['사과', 'banana', 'orange','포도'];
 
-const myList = myArray.map((item) => <p>{item}</p>)
+const myList = myArray.map((item) => <p>{item}</p>);
 
-ReactDOM.render(myList, document.getElementById('root'));
+ReactDOM.render(myList, document.getElementById('root'));*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
